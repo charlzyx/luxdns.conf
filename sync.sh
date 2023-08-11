858fcbb9e60632df88593d484667d019678e8c08
charlzyx <charlzyx@qq.com>
2023-08-11 11:34:58 +0800

    auto update

--- sync.sh
+++ sync.sh
@@ -1,11 +1,12 @@
 rm -rf ./mosdns
 rm -rf ./AdguardHome.yaml
-rm -rf ./resolve.conf
 mkdir -p ./mosdns
 
 cp /etc/mosdns/config.yaml ./mosdns/
 cp /etc/mosdns/*.sh ./mosdns/
 cp /opt/AdGuardHome/AdGuardHome.yaml ./
+cp ~/.zshrc ./
+
 
 git add .
 git commit -m 'auto update'
